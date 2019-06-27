a4ff6ed3f7595dc6837d04dff15243a765c355de
Paul Nelson <pmnelso@gmail.com>
2019-06-27 19:05:56 -0400

    Updates

---
+++ precache-manifest.4c77d3a2125907b3b9abed5379e95eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dea8d3dea2056a929c6e1af2cd1cb5",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "521822322f33768a53a2",
+    "url": "/Clicky-Game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Clicky-Game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "521822322f33768a53a2",
+    "url": "/Clicky-Game/static/js/main.f8ee1fb8.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
